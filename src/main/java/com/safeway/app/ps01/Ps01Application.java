3fd017a51980900536003ef630ad23c819a32f7a
Ryan Saplan <ryan.saplan@safeway.com>
2019-09-30 07:35:38 +0800

    Initial spring boot setup for ps01

---
+++ src/main/java/com/safeway/app/ps01/Ps01Application.java
@@ -0,0 +1,13 @@
+package com.safeway.app.ps01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps01Application.class, args);
+	}
+
+}
